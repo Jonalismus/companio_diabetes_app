18456699ac124b9cd2a14b448afc18c7bfa828bb
s0582382 <Fenstersteuerungber Support Annalena.Stamp@Student.HTW-Berlin.de>
2023-12-10 16:42:39 +0100

    Änderungen der Notfallplanpage

--- lib/screens/pages/notfallplan.dart
+++ lib/screens/pages/notfallplan.dart
@@ -16,44 +16,98 @@ class NotfallplanPage extends StatelessWidget {
         ),
         automaticallyImplyLeading: false,
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               'Falls es zu einer Über- oder Unterzuckerung kommt, befolgen Sie diese Anweisungen:',
               style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 20),
-            Text(
+            const Text(
               'Schritt 1: Besteht die Gefahr von Bewusstlosigkeit?\n'
-                  'In diesem Fall wählen Sie bitte umgehend die Notfallhotline 112 oder 116 117.',
+                  'Wählen Sie umgehend die Notfallhotline 112 oder 116 117.',
               style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 20),
-            Text('Schritt 2: Messen Sie Ihren Blutzucker.',
+            const Text(
+              'Schritt 2: Messen Sie Ihren Blutzucker und klären Sie ob es sich um eine Über- oder Unterzuckerung handelt.',
               style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 20),
-            Text('Schritt 3: Klären Sie, ob es sich um eine Über- oder Unterzuckerung handelt.',
-              style: TextStyle(fontSize: 20),
-            ),
-            Text('Unterzuckerung:\n'
-                '- Konsumieren Sie etwas Zuckerhaltiges wie zum Beispiel Fruchtsaft oder Traubenzucker \n'
-                '- Verwenden Sie, falls vorhanden, eine Glukagon Spritze\n'
-                '- Messen Sie den Blutzucker nach 15 min nochmal und beurteilen Sie den Gesundheitszustand',
-              style: TextStyle(fontSize: 20),
+            ElevatedButton(
+              onPressed: () {
+                _showUnterzuckerungInfo(context);
+              },
+              child: Text('Unterzuckerung'),
             ),
             SizedBox(height: 20),
-            Text('Überzuckerung:\n'
-                '- Trinken Sie viel Wasser\n'
-                '- Spritzen Sie sich Insulin',
-              style: TextStyle(fontSize: 20),
+            ElevatedButton(
+              onPressed: () {
+                _showUeberzuckerungInfo(context);
+              },
+              child: Text('Überzuckerung'),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _showUnterzuckerungInfo(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Unterzuckerung'),
+          content: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('- Konsumieren Sie etwas Zuckerhaltiges wie zum Beispiel Fruchtsaft oder Traubenzucker'),
+              Text('- Verwenden Sie, falls vorhanden, eine Glukagon Spritze'),
+              Text('- Messen Sie den Blutzucker nach 15 min nochmal und beurteilen Sie den Gesundheitszustand'),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Schließen'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showUeberzuckerungInfo(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Überzuckerung'),
+          content: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('- Trinken Sie viel Wasser'),
+              Text('- Spritzen Sie sich Insulin'),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Schließen'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
