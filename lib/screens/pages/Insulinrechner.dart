74b10ad81764b09b1b08ec6e69d018611d5ec654
s582015 <kateryna.tishchenko@student.htw-berlin.de>
2023-12-27 17:04:05 +0100

    Insulinrechner scratch

--- lib/screens/pages/Insulinrechner.dart
+++ lib/screens/pages/Insulinrechner.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Services/insulinCalculator.dart';
 
 class Insulinrechner extends StatefulWidget {
   const Insulinrechner({Key? key}) : super(key: key);
@@ -9,24 +10,58 @@ class Insulinrechner extends StatefulWidget {
 
 
 class _InsulinrechnerState extends State<Insulinrechner> {
-  TextEditingController _controller = TextEditingController();
-  String _warningMessage = '';
-  bool pumpe = true; // Change this value
+  TextEditingController _controllerGlycose = TextEditingController();
+  TextEditingController _controllerCarbohydrates = TextEditingController();
+
+  String _warningMessageOne = '';
+  String _warningMessageTwo = '';
+  double totalUnitsVal = 60;
+  double basalPercentageVal = 0.4;
+  double bolusPercentageVal = 0.6;
+  int mealsPerDayVal = 3;
+  double afterMealTargetGlucoseVal = 90;
+  bool pumpe = false; // Change this value
+  late double bloodSugarValue = 60; //get from database in future
+  late double carbohydrates;
+  late double insulinUnits = 0;
 
   void _checkBloodSugar() {
-    if (_controller.text.isEmpty) {
+    if (_controllerGlycose.text.isEmpty) {
       setState(() {
-        _warningMessage = 'Bitte geben Sie einen Blutwert ein.';
+        _warningMessageOne = 'Geben Sie einen Blutwert ein.';
       });
       return;
     } else {
-      double bloodSugarValue = double.tryParse(_controller.text) ?? 0;
-      _calculateInsulinBedarf(bloodSugarValue);
+      bloodSugarValue = double.tryParse(_controllerGlycose.text) ?? 0;
     }
   }
 
-  void _calculateInsulinBedarf(double bloodSugarValue) {
+  void _checkCarbohydrates() {
+    if (_controllerCarbohydrates.text.isEmpty) {
+      setState(() {
+        _warningMessageTwo = 'Geben Sie Ihren Kohlenhydratwert ein.';
+      });
+      return;
+    } else {
+      carbohydrates = double.tryParse(_controllerCarbohydrates.text) ?? 0;
+    }
+  }
 
+  double calculateInsulinBedarf(double bloodSugarValue, carbsIntake) {
+      InsulinCalculator insulinCalc = InsulinCalculator(
+          totalUnitsVal,
+          basalPercentageVal,
+          bolusPercentageVal,
+          mealsPerDayVal,
+          afterMealTargetGlucoseVal);
+
+      double corrFactor = InsulinCalculator.getCorrectionFactor(
+          totalUnitsVal, true, false);
+      double premealCorrUnits = insulinCalc.getPremealCorrectionUnits(
+          bloodSugarValue, "rapid", corrFactor);
+      double afterMealInsulin = insulinCalc.getAfterMealInsulin(
+          carbsIntake, premealCorrUnits);
+      return afterMealInsulin;
   }
 
   @override
@@ -55,8 +90,7 @@ class _InsulinrechnerState extends State<Insulinrechner> {
                     width: MediaQuery.of(context).size.width * 0.7,
                     alignment: Alignment.center,
                     color: Colors.redAccent,
-                    child: const Text(
-                      '67', // mock for blood sugar value
+                    child: Text('$bloodSugarValue', // mock for blood sugar value
                       style: TextStyle(
                         fontSize: 90.0,
                         color: Colors.white,
@@ -70,16 +104,106 @@ class _InsulinrechnerState extends State<Insulinrechner> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextField(
-                    controller: _controller,
+                    controller: _controllerGlycose,
                     keyboardType: const TextInputType.numberWithOptions(decimal: true),
                     decoration: const InputDecoration(
                       labelText: 'Aktuellen Blutwert (mg/dl) eingeben',
                     ),
                   ),
+                  const SizedBox(height: 13),
+                  ElevatedButton(
+                    onPressed: _checkBloodSugar,
+                    child: const Text('Blutwert überprüfen'),
+                  ),
                   const SizedBox(height: 16),
+                  Text(
+                    _warningMessage,
+                    style: const TextStyle(
+                      color: Colors.red,
+                      fontSize: 16,
+                    ),
+                  ),
                 ],
               ),
-          ],
+                  Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  TextField(
+                  controller: _controllerCarbohydrates,
+                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                  decoration: const InputDecoration(
+                  labelText: 'Kohlenhydrate in Gramm: ',
+                  ),
+                  ),
+                  const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        _checkCarbohydrates;
+                        insulinUnits = calculateInsulinBedarf(bloodSugarValue, carbohydrates);
+                          Column(mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Benötigte Insulin Units: ',
+                                style: TextStyle(
+                                  color: Colors.indigo,
+                                  fontSize: 22,
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.7,
+                                alignment: Alignment.center,
+                                color: Colors.redAccent,
+                                child: Text('$insulinUnits', // mock for blood sugar value
+                                  style: TextStyle(
+                                    fontSize: 90.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],);
+                        },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.deepPurple,
+                      ),
+                      child: const Text('Insulinbedarf berechnen'),
+                    ),
+                  const SizedBox(height: 13),
+                  Text(
+                  _warningMessage,
+                  style: const TextStyle(
+                  color: Colors.red,
+                  fontSize: 12,
+                  ),
+                  ),
+                    const SizedBox(height: 1),
+                  ],
+               ),
+              if (insulinUnits>0)
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Benötigte Insulin Units: ',
+                      style: TextStyle(
+                        color: Colors.indigo,
+                        fontSize: 22,
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      alignment: Alignment.center,
+                      color: Colors.redAccent,
+                      child: Text('$insulinUnits', // mock for blood sugar value
+                        style: TextStyle(
+                          fontSize: 90.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+             ],
         ),
       ),
     );
