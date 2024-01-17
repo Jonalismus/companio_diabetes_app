e88749d5ab93395485c59e5cd3d8afb006ab85fe
zhang-weijie <zhangweijiepku@gmail.com>
2024-01-17 22:57:49 +0100

    Ernährungstagebuch mit Einstellungen ersetzt

--- lib/screens/pages/sugarIntakeOverview.dart
+++ lib/screens/pages/sugarIntakeOverview.dart
@@ -29,14 +29,16 @@ class _SugarIntakeOverviewState extends State<SugarIntakeOverview> {
   @override
   void initState() {
     super.initState();
-    selectedDate = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+    selectedDate =
+        DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
   }
 
   List<SugarIntakeData> _generateChartData(DateTime centerDate) {
     Map<DateTime, SugarIntakeData> sugarIntakeMap = {};
 
     for (var mealEntry in mealEntries) {
-      DateTime date = DateTime(mealEntry.dateTime.year, mealEntry.dateTime.month, mealEntry.dateTime.day);
+      DateTime date = DateTime(mealEntry.dateTime.year,
+          mealEntry.dateTime.month, mealEntry.dateTime.day);
 
       sugarIntakeMap.putIfAbsent(date, () => SugarIntakeData(date: date));
 
@@ -49,12 +51,12 @@ class _SugarIntakeOverviewState extends State<SugarIntakeOverview> {
     List<SugarIntakeData> chartData = [];
     for (int i = -3; i <= 3; i++) {
       DateTime currentDate = centerDate.add(Duration(days: i));
-      chartData.add(sugarIntakeMap[currentDate] ?? SugarIntakeData(date: currentDate));
+      chartData.add(
+          sugarIntakeMap[currentDate] ?? SugarIntakeData(date: currentDate));
     }
 
     return chartData;
   }
-
 
   void _changeDate(bool increment) {
     setState(() {
@@ -68,17 +70,16 @@ class _SugarIntakeOverviewState extends State<SugarIntakeOverview> {
   @override
   Widget build(BuildContext context) {
     DataProvider dataProvider =
-    Provider.of<DataProvider>(context, listen: true);
+        Provider.of<DataProvider>(context, listen: true);
     if (!dataProvider.isLoaded) {
       return const Center(child: CircularProgressIndicator());
     }
     mealEntries = dataProvider.mealEntries;
-
     chartData = _generateChartData(selectedDate);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Zuckeraufnahmenübersicht'),
+        title: const Text('Kohlenhydrataufnahmenübersicht'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -111,7 +112,7 @@ class _SugarIntakeOverviewState extends State<SugarIntakeOverview> {
                 dateFormat: DateFormat.Md(),
               ),
               primaryYAxis: NumericAxis(
-                title: AxisTitle(text: 'Zuckeraufnahme (g)'),
+                title: AxisTitle(text: 'Kohlenhydrataufnahme (g)'),
                 plotBands: <PlotBand>[
                   PlotBand(
                     isVisible: true,
@@ -125,20 +126,20 @@ class _SugarIntakeOverviewState extends State<SugarIntakeOverview> {
               ),
               series: <CartesianSeries>[
                 ColumnSeries<SugarIntakeData, DateTime>(
-                  dataSource: chartData,
-                  xValueMapper: (SugarIntakeData data, _) => data.date,
-                  yValueMapper: (SugarIntakeData data, _) => data.totalSugarIntake,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  color: Colors.red
-                ),
+                    dataSource: chartData,
+                    xValueMapper: (SugarIntakeData data, _) => data.date,
+                    yValueMapper: (SugarIntakeData data, _) =>
+                        data.totalSugarIntake,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Colors.red),
               ],
             ),
             Padding(
               padding: const EdgeInsets.only(top: 16.0),
               child: Text(
-                "Diese Tabelle zeigt Ihre tägliche Zuckeraufnahme. Der grüne Bereich markiert den sinnvollen Wertebereich.",
-                    // "Ein Erwachsener mit moderater Aktivität, der täglich 2000-2500 Kalorien benötigt, sollte 45%-65% dieser Kalorien in Form von Kohlenhydraten zu sich nehmen. "
-                    // "Dies entspricht ungefähr 281-406 Gramm Kohlenhydraten pro Tag.",
+                "Diese Tabelle zeigt Ihre tägliche Kohlenhydrataufnahme. Der grüne Bereich markiert den sinnvollen Wertebereich.",
+                // "Ein Erwachsener mit moderater Aktivität, der täglich 2000-2500 Kalorien benötigt, sollte 45%-65% dieser Kalorien in Form von Kohlenhydraten zu sich nehmen. "
+                // "Dies entspricht ungefähr 281-406 Gramm Kohlenhydraten pro Tag.",
                 textAlign: TextAlign.left,
                 style: TextStyle(
                   fontSize: 15,
@@ -146,7 +147,40 @@ class _SugarIntakeOverviewState extends State<SugarIntakeOverview> {
                 ),
               ),
             ),
-            // Other widgets...
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.center,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChangeNotifierProvider(
+                            create: (context) => DataProvider()..loadData(),
+                            child: FoodDairyPage(),
+                          ),
+                        )).then((newMealEntries) {
+                      // if (newMealEntries != null) {
+                        setState(() {
+                          DataProvider dataProvider =
+                              Provider.of<DataProvider>(context, listen: true);
+                          if (dataProvider.isLoaded) {
+                            // mealEntries = newMealEntries;
+                            mealEntries = [];
+                            chartData = _generateChartData(selectedDate);
+                          }
+                        });
+                      // }
+                    });
+                  },
+                  child: Text("Mein Ernährungstagebuch"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, // Hintergrundfarbe des Buttons
+                    onPrimary: Colors.white, // Textfarbe des Buttons
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
